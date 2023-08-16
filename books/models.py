c9ad05f38e29427b2bfb7ce4f0dd47bfa03c53f2
barzinprograms@gmail.com <barzinprograms@gmail.com>
2023-08-16 12:31:04 +0330

    add media

--- books/models.py
+++ books/models.py
@@ -12,6 +12,8 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6,decimal_places=2)
+    cover = models.ImageField(upload_to='covers/',default='covers/default.jpeg')
+    
 
     def __str__(self) -> str:
         return self.title
--- config/settings.py
+++ config/settings.py
@@ -163,6 +163,10 @@ STATICFILES_FINDERS = [
 ]
 
 
+MEDIA_URL ='/media/'
+MEDIA_ROOT = str(BASE_DIR/'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
