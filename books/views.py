ef39fad6b9be6dcffbdadb1d513f43c2be828a28
barzinprograms@gmail.com <barzinprograms@gmail.com>
2023-08-18 18:21:08 +0330

    add searchbar

--- books/views.py
+++ books/views.py
@@ -1,3 +1,6 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.db.models import Q 
 from .models import Book
 from django.views.generic import ListView,DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin , PermissionRequiredMixin
@@ -13,4 +16,14 @@ class BookDetailView( LoginRequiredMixin,PermissionRequiredMixin ,DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     permission_required = 'books.special_status'
-    #permission_denied_message = 'you have not access to this page'
+
+class SearchResultListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    
+    def get_queryset(self): 
+        query = self.request.GET.get('q','')
+        return Book.objects.filter(
+        Q(title__icontains=query) | Q(author__icontains=query)
+        )
